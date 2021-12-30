87ba20cb210050896ad4f404d3d521f7f6f4a748
masterkey-07 <victorchaves03@gmail.com>
2021-12-30 09:34:07 -0300

    update

--- server/server.js
+++ server/server.js
@@ -13,11 +13,11 @@ app.use(express.json({ extended: false }));
 
 app.use("/api/", require("./api/"));
 
-/*app.use(express.static("../client/build"));
+app.use(express.static("../client/build"));
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
-});*/
+});
 
 const PORT = process.env.PORT || 5000;
 
